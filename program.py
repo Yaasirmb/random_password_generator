498a45e045a1b17b639853b0b1877e8b0156e7fb
Yaasir Mahamadou-Bachirou <yaasir.mahamadou@gmail.com>
2021-02-13 14:42:51 -0500

    User input is now handled in this file. Also it's generating 1 million passwords just for testing purposes.

--- program.py
+++ program.py
@@ -1,6 +1,15 @@
-from password_generator import user
+from password_generator import password
+from password_generator import all_chars
+import random
+#print('What would you like your password to contain?\n')
 
-while user() != 'Done':
-    user()
-else:
-    print('***GENERATING PASSWORD***')
+#user_pass_chars = input("""Enter 'Uppercase' for uppercase letters, 'Lowercase' for lowercase letters, or 'Both' for both,
+#'Digits' for numbers, and 'Punctuations' for symbols (ex: @#$%^&), and 'All Characters' for all characters. \n""")
+
+#user_pass_len = input('How long would you like your password to be? (Enter a number ex: 12) \n')
+#all_chars,k=random.randrange(8,22)
+
+chars = ''
+pass_len = ''
+for _ in range(0,1000000):
+    password(chars,pass_len)
