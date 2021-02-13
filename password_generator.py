4bc82a1c59b3c1ca0a13ab5468bd0cbfe8530600
Yaasir Mahamadou-Bachirou <yaasir.mahamadou@gmail.com>
2021-02-13 03:21:57 -0500

    Changed 'all_chars' to not include whitespace characters. Function also generates a random password for you if no values are entered.

--- password_generator.py
+++ password_generator.py
@@ -6,16 +6,18 @@ lower_letters = string.ascii_lowercase
 upper_letters = string.ascii_uppercase
 digits = string.digits
 punctuations = string.punctuation
-all_chars = string.printable
+# Esentially == string.printable, but without the whitespace characters.
+all_chars = ''.join(string.ascii_letters + string.digits + string.punctuation)
 
 print('What would you like your password to contain?\n')
+
 user_pass_chars = input("""Enter 'Uppercase' for uppercase letters, 'Lowercase' for lowercase letters, or 'Both' for both,
 'Digits' for numbers, and 'Punctuations' for symbols (ex: @#$%^&), and 'All Characters' for all characters. \n""")
 
 user_pass_len = input('How long would you like your password to be? (Enter a number ex: 12) \n')
 
 def password(pass_chars,pass_len):
-
+    """ Function that generates a pseudo random password of a desired length and desired characters. """
     if pass_chars == 'Uppercase':
         print(''.join(random.choices(upper_letters,k=int(pass_len))))
     elif pass_chars == 'Lowercase':
@@ -28,7 +30,10 @@ def password(pass_chars,pass_len):
         print(''.join(random.choices(punctuations,k=int(pass_len))))
     elif pass_chars == 'All Characters':
         print(''.join(random.choices(all_chars,k=int(pass_len))))
+    elif pass_chars == '' and pass_len == '':
+        print("You didn't enter any values, so here's a random password I made for you :D \n")
+        print(''.join(random.choices(all_chars,k=random.randrange(8,22))))
     else:
-        print('Sorry please choose one of the available options.')
+        print('Sorry, please choose one of the available options.')
 
 password(user_pass_chars,user_pass_len)
