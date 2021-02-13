11a24b56dc16f71d634366aa907c4e2a4ebc1697
Yaasir Mahamadou-Bachirou <yaasir.mahamadou@gmail.com>
2021-02-13 02:17:32 -0500

    Rewrote program, it now uses native string and random modules to generate a pseudo random password.

--- password_generator.py
+++ password_generator.py
@@ -1,53 +1,34 @@
-letters = ['A','B','C','D','E']
-user_options = []
-options = 'Letters(upper and lowercase), numbers, and special symbols.'
-password_options = ["Letters","Numbers","Symbols"]    
-symbols = [' ','!','"','#','$','%','&',"'",'(',')','*',
-    '+',',','-','.','/',':',';','<','=',">",'?','@','['
-    ,']','^','_','`','{','|','}','~',']',"\\"]
+import string
+import random
 
-def user():
-    """Take the 's user_input."""
-    while True:
-        print ('Your password can contain all or some of the following: Letters(upper and lowercase), numbers, and special symbols.')
-        print('\n')
-        user_input = input("Please type what you would like to include in your password.\n").title()
-        if user_input in password_options:
-            user_options.append(user_input)
-            print('Your current options are ' + str(user_options))
+all_letters = string.ascii_letters
+lower_letters = string.ascii_lowercase
+upper_letters = string.ascii_uppercase
+digits = string.digits
+punctuations = string.punctuation
+all_chars = string.printable
 
-        elif user_input not in password_options:
-            print('This item is not a valid option.\n')
+print('What would you like your password to contain?\n')
+user_pass_chars = input("""Enter 'Uppercase' for uppercase letters, 'Lowercase' for lowercase letters, or 'Both' for both,
+'Digits' for numbers, and 'Punctuations' for symbols (ex: @#$%^&), and 'All Characters' for all characters. \n""")
 
-        elif user_input == 'Done' and len(user_options) <= 3:
-            for option in user_options:
-                    print(option)
-        elif len(user_options) > 3:
-            print("You can only choose 3 options.")
-            print('Letters(upper and lowercase), numbers, and special symbols.')
-            print('\n')           
+user_pass_len = input('How long would you like your password to be? (Enter a number ex: 12) \n')
 
-        elif user_options == 'Done' and len(user_options) == 0:
-            print('Good Bye.')
-            
-        elif user_input == 'Option':
-            print(options)
-            
-        elif user_input == 'Remove':
-            remove_option = input('Here is your current user_input ' + str(user_options) + ' what would you like to remove? \n').title()
-            if remove_option in user_input:
-                user_options.remove(remove_option)
-                print(str(user_options) + '\n')
-            else:
-                print("Sorry this option is not among the options you chose. You currently have: " + str(user_input) + '\n' )
-        return(user_input)
+def password(pass_chars,pass_len):
 
+    if pass_chars == 'Uppercase':
+        print(''.join(random.choices(upper_letters,k=int(pass_len))))
+    elif pass_chars == 'Lowercase':
+        print(''.join(random.choices(lower_letters,k=int(pass_len))))
+    elif pass_chars == 'Both':
+        print(''.join(random.choices(all_letters,k=int(pass_len))))
+    elif pass_chars == 'Digits':
+        print(''.join(random.choices(digits,k=int(pass_len))))
+    elif pass_chars == 'Punctuations':
+        print(''.join(random.choices(punctuations,k=int(pass_len))))
+    elif pass_chars == 'All Characters':
+        print(''.join(random.choices(all_chars,k=int(pass_len))))
+    else:
+        print('Sorry please choose one of the available options.')
 
-"""def random_number():
-    randrange(10)
-
-def random_symbol():
-    
-
-def random_letter():
-    print(random.sample(letters,2))"""
+password(user_pass_chars,user_pass_len)
