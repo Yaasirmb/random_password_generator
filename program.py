afe9ef0c837cfc9c9e90c69fc20d70299c08e583
Yaasirmb <53307955+Yaasirmb@users.noreply.github.com>
2021-02-13 23:51:18 -0500

    Removed unnecessary code.

--- program.py
+++ program.py
@@ -1,13 +1,9 @@
 from password_generator import password
-from password_generator import all_chars
-import random
-#print('What would you like your password to contain?\n')
 
+#print('What would you like your password to contain?\n')
 #user_pass_chars = input("""Enter 'Uppercase' for uppercase letters, 'Lowercase' for lowercase letters, or 'Both' for both,
 #'Digits' for numbers, and 'Punctuations' for symbols (ex: @#$%^&), and 'All Characters' for all characters. \n""")
-
 #user_pass_len = input('How long would you like your password to be? (Enter a number ex: 12) \n')
-#all_chars,k=random.randrange(8,22)
 
 chars = ''
 pass_len = ''
