e7c03b0ce1608f7a310ddafe80f46e07ca05827d
Yaasir Mahamadou-Bachirou <yaasir.mahamadou@gmail.com>
2021-02-15 02:15:09 -0500

    Testing the execution time for generating 1 mil passwords.

--- program.py
+++ program.py
@@ -1,11 +1,16 @@
 from password_generator import password
+import time
 
 #print('What would you like your password to contain?\n')
-user_pass_chars = input("""Enter 'Uppercase' for uppercase letters, 'Lowercase' for lowercase letters, or 'Both' for both,
-'Digits' for numbers, and 'Punctuations' for symbols (ex: @#$%^&), and 'All Characters' for all characters. \n""")
-user_pass_len = input('How long would you like your password to be? (Enter a number ex: 12) \n')
+#user_pass_chars = input("""Enter 'Uppercase' for uppercase letters, 'Lowercase' for lowercase letters, or 'Both' for both,
+#'Digits' for numbers, and 'Punctuations' for symbols (ex: @#$%^&), and 'All Characters' for all characters. \n""")
+#user_pass_len = input('How long would you like your password to be? (Enter a number ex: 12) \n')
 
-#chars = ''
-#pass_len = ''
-#for _ in range(10):
-password(user_pass_chars,user_pass_len)
+start_time = time.time()
+chars = ''
+pass_len = ''
+
+for _ in range(1000000):
+    password(chars,pass_len)
+
+print("Secrets module took: --- %s seconds ---" % (time.time() - start_time))
